2bdab09ab5c0659a73704f27b49c965b7be3fc59
rizalajih <rizalaji26@gmail.com>
2022-04-12 14:03:13 +0700

    new

--- app/Views/Signin.php
+++ app/Views/Signin.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Codeigniter Login with Email/Password Example</title>
+    <title>Log in Data Barang</title>
   </head>
   <body>
     <div class="container">
--- app/Views/Signup.php
+++ app/Views/Signup.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Codeigniter Auth User Registration Example</title>
+    <title>Sign up Data Barang</title>
 </head>
 <body>
     <div class="container mt-5">
